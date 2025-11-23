1c1e8dc060f7c70680fd50615ec499ce634584bd
Felipe Teixeira de Lima <felipe.teixeiralima2511@gmail.com>
2025-11-23 14:53:55 -0300

    Secret Key escondida

--- projeto_final/settings.py
+++ projeto_final/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v9bq*&4hanbvof!%4^kmvri1$4i3(4hi7f#d6f6kmh7ach=^!7'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
